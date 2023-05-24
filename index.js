7248333eea0f090a42156779ca4b24fe4a99b606
mbron770 <mbronfin.tsd@gmail.com>
2023-05-23 21:21:24 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+
+
+main.remove(); 
+// main.remove() removes the main element 
+// main.remove() removes main 
+// main.remove() removes main  
+// main.remove() removes main 
+
+
+
+
+
+const newHeader = document.createElement('h1')
+// document.createElement('h1') creates element
+// document.createElement('h1) creates element
+// document.createElement('h2) creates elemnebn
+// document.createElement('h1") creates element 
+
+newHeader.id = 'victory'
+
+newHeader.textContent = 'Mordechai is the champion' 
